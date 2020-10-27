af0583af7f03a6028b91294298f5f759a0e20fe2
krash_777@mail.ru <Abracadabra224145>
2020-10-27 21:15:11 +0400

    time - design

--- webpack.config.js
+++ webpack.config.js
@@ -6,27 +6,15 @@ module.exports = {
                  bundle: './src/app.js'
             },
     output: { path: __dirname, filename: 'index.js' },
-    // output: {
-    //         path: path.resolve(__dirname, 'game'),
-    //         filename: 'index.js',
-    //         //publicPath:  path.resolve(__dirname, './game/'),
-    //         },
 
     module: {
 
-        // loaders: [
-        //     {
-        //         test: /.jsx?$/,
-        //         loader: 'babel-loader',
-        //         exclude: /node_modules/,
-        //         query: {
-        //             presets: ['es2015', 'react']
-        //         }
-        //     },
-        //     {
-        //         test: /\.css$/,
-        //         use: ['style-loader', 'css-loader']
-        //     },
+        loaders:[
+
+            { test: /\.(eot|svg|ttf|woff|woff2)$/, loader: 'file?name=[name].[ext]'},
+
+        ],
+
         rules: [
             {
                 test: /.jsx?$/,
@@ -40,23 +28,10 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
             },
-
-
-
-            // {
-            //     test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
-            //     use: {
-            //         loader: 'file-loader',
-            //         options: {
-            //             name: '[path]/[name].[ext]', // указываем, куда собираются картинки / имя . расширение
-            //         }
-            //     }
-            // }
         ]
     },
+
     watch: true,
-    // watchOptions: {
-    //     poll: 1000 // Check for changes every second
-    // },
+
     mode: 'development',
 };
